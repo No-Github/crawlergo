f575f3cbf0cb0a2bdc1f8e395ff3929c5b78258e
r0fus0d <d2hwakh7bs5e@protonmail.com>
2024-01-03 16:56:03 +0800

    update 2024年 1月 3日 星期三 16时56分03秒 CST

--- get_chrome.sh
+++ get_chrome.sh
@@ -1,29 +1,20 @@
+# 修改版
+# 存到 /root/.cache/rod/browser/chromium-*
 #!/bin/bash
-cd $(dirname $0)
-
 LASTCHANGE_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media"
-
 REVISION=$(curl -s -S $LASTCHANGE_URL)
 
 echo "latest revision is $REVISION"
 
-if [ -d $REVISION ] ; then
-  echo "already have latest version"
-  exit
-fi
-
 ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
-
-ZIP_FILE="${REVISION}-chrome-linux.zip"
+ZIP_FILE="${REVISION}chrome-linux.zip"
 
 echo "fetching $ZIP_URL"
 
-rm -rf $REVISION
-mkdir $REVISION
-pushd $REVISION
-curl -# $ZIP_URL > $ZIP_FILE
+mkdir -p /root/.cache/rod/browser/
+cd /root/.cache/rod/browser/
+curl -o /root/.cache/rod/browser/$ZIP_FILE $ZIP_URL
 echo "unzipping.."
+cd /root/.cache/rod/browser/
 unzip $ZIP_FILE
-popd
-rm -f ./latest
-ln -s $REVISION/chrome-linux/ ./latest
+mv chrome-linux chromium-${REVISION}
